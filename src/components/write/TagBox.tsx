a485c3a273177b1e99d778f5425dc799c228a064
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2024-10-26 23:41:01 +0900

    [feat] 해시태그 추가와 글쓰기 버튼 구현
    해시태그 추가와 글쓰기 버튼 구현

--- src/components/write/TagBox.tsx
+++ src/components/write/TagBox.tsx
@@ -1,41 +1,72 @@
 import '../../styles/board/TagBox.scss';
-import React from 'react';
+import React, { ChangeEvent, FormEvent, FormEventHandler, useCallback, useState } from 'react';
 
 const TagBox = () => {
+
+  const [input, setInput] = useState('');
+  const [tags, setTags] = useState<string[]>([]);
+
+  const insertTag = useCallback((tag: string) => {
+    if (tag && !tags.includes(tag)) {
+      setTags([...tags, tag]);
+    }
+  }, [tags]);
+
+  const onRemove = useCallback((tag: string) => {
+    setTags(tags.filter(t => t !== tag));
+  }, [tags]);
+
+  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback((e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    insertTag(input.trim());
+    setInput('');
+  }, [insertTag, input]);
+
   return (
     <div className={`tag-box-block`}>
       <h4>태그</h4>
-      <form className={`tag-form`}>
-        <input placeholder={`태그를 입력하세요`} />
+      <form className={`tag-form`} onSubmit={onSubmit}>
+        <input placeholder={`태그를 입력하세요`} value={input} onChange={onChange} />
         <button type={`submit`}>추가</button>
       </form>
-      <TagList tags={[{tag: '태그1'}, {tag: '태그2'}, {tag: 'tag3'}]}/>
+      <TagList tags={tags} onRemove={onRemove} />
     </div>
   );
 };
 
-const TagList: React.FC<TagListProps> = ({tags}) => {
+const TagList: React.FC<TagListProps> = ({ tags, onRemove }) => {
   return (
     <div className={`tag-list`}>
-      {tags.map(item => (
-        <TagItem key={item.tag} tag={item.tag} />
+      {tags.map(tag => (
+        <TagItem key={tag} tag={tag} onRemove={onRemove} />
       ))}
     </div>
   );
 };
 
-const TagItem: React.FC<TagItemProps> = ({ tag }) => {
+const TagItem: React.FC<TagItemProps> = ({ tag, onRemove }) => {
+
+  const onClick = useCallback(() => {
+    onRemove(tag);
+  }, [onRemove, tag]);
+
   return (
-    <div className={`tag`}>#{tag}</div>
+    <div className={`tag`} onClick={onClick}>#{tag}</div>
   );
 };
 
 type TagListProps = {
-  tags: TagItemProps[]
+  tags: string[];
+  onRemove: (tag: string) => void;
 }
 
 type TagItemProps = {
   tag: string;
+  onRemove: (tag: string) => void;
 };
 
 export default TagBox;
--- src/pages/board/BoardWritePage.tsx
+++ src/pages/board/BoardWritePage.tsx
@@ -1,12 +1,14 @@
 import Editor from '../../components/write/Editor';
 import TagBox from '../../components/write/TagBox';
 import Responsive from '../../components/Responsive';
+import WriteActionButtons from '../../components/write/WriteActionButtons';
 
 const BoardWritePage = () => {
   return (
     <Responsive>
       <Editor />
       <TagBox />
+      <WriteActionButtons/>
     </Responsive>
   );
 };
